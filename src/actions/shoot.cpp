c0c3cb38e6390bbab7b1222937bdb7ced80c7775
nonk123 <me@nonk.dev>
2023-10-17 10:05:00 +0300

    Fix shooting at point-blank range

--- src/actions/shoot.cpp
+++ src/actions/shoot.cpp
@@ -6,8 +6,8 @@ void Shoot::perform(Thing& actor) {
   projectile->trajectory =
       rasterize(actor.x, actor.y, projectile->dest_x, projectile->dest_y);
 
-  projectile->x = projectile->trajectory[1].x;
-  projectile->y = projectile->trajectory[1].y;
+  projectile->x = projectile->trajectory[0].x;
+  projectile->y = projectile->trajectory[0].y;
 
   spawn_queue.push_back(std::move(projectile));
   actor.play_sound_local(rifle_fire);
--- src/things/projectile.cpp
+++ src/things/projectile.cpp
@@ -43,6 +43,4 @@ void Projectile::act() {
   }
 }
 
-std::vector<TintedSprite> Bullet::draw() {
-  return {Sprite::BULLET};
-}
+std::vector<TintedSprite> Bullet::draw() { return {Sprite::BULLET}; }
